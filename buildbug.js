5570d7659464cb10ab92c8080867990ccc498664
Brian Neisler <neisler.brian@gmail.com>
2014-05-23 10:21:39 -0700

    Update buildbug to match new createNodePackage signature

--- buildbug.js
+++ buildbug.js
@@ -168,14 +168,16 @@ buildTarget('local').buildFlow(
                 targetTask('createNodePackage', {
                     properties: {
                         packageJson: buildProject.getProperty("node.packageJson"),
-                        readmePath: buildProject.getProperty("node.readmePath"),
-                        sourcePaths: buildProject.getProperty("node.sourcePaths").concat(
-                            buildProject.getProperty("node.unitTest.sourcePaths")
-                        ),
-                        scriptPaths: buildProject.getProperty("node.scriptPaths").concat(
-                            buildProject.getProperty("node.unitTest.scriptPaths")
-                        ),
-                        testPaths: buildProject.getProperty("node.unitTest.testPaths")
+                        packagePaths: {
+                            ".": [buildProject.getProperty("node.readmePath")],
+                            "./lib": buildProject.getProperty("node.sourcePaths").concat(
+                                buildProject.getProperty("node.unitTest.sourcePaths")
+                            ),
+                            "./scripts": buildProject.getProperty("node.scriptPaths").concat(
+                                buildProject.getProperty("node.unitTest.scriptPaths")
+                            ),
+                            "./test": buildProject.getProperty("node.unitTest.testPaths")
+                        }
                     }
                 }),
                 targetTask('generateBugPackRegistry', {
@@ -313,13 +315,15 @@ buildTarget('prod').buildFlow(
                 targetTask('createNodePackage', {
                     properties: {
                         packageJson: buildProject.getProperty("node.unitTest.packageJson"),
-                        sourcePaths: buildProject.getProperty("node.sourcePaths").concat(
-                            buildProject.getProperty("node.unitTest.sourcePaths")
-                        ),
-                        scriptPaths: buildProject.getProperty("node.scriptPaths").concat(
-                            buildProject.getProperty("node.unitTest.scriptPaths")
-                        ),
-                        testPaths: buildProject.getProperty("node.unitTest.testPaths")
+                        packagePaths: {
+                            "./lib": buildProject.getProperty("node.sourcePaths").concat(
+                                buildProject.getProperty("node.unitTest.sourcePaths")
+                            ),
+                            "./scripts": buildProject.getProperty("node.scriptPaths").concat(
+                                buildProject.getProperty("node.unitTest.scriptPaths")
+                            ),
+                            "./test": buildProject.getProperty("node.unitTest.testPaths")
+                        }
                     }
                 }),
                 targetTask('generateBugPackRegistry', {
@@ -359,9 +363,11 @@ buildTarget('prod').buildFlow(
                 targetTask('createNodePackage', {
                     properties: {
                         packageJson: buildProject.getProperty("node.packageJson"),
-                        readmePath: buildProject.getProperty("node.readmePath"),
-                        sourcePaths: buildProject.getProperty("node.sourcePaths"),
-                        scriptPaths: buildProject.getProperty("node.scriptPaths")
+                        packagePaths: {
+                            ".": buildProject.getProperty("node.readmePath"),
+                            "./lib": buildProject.getProperty("node.sourcePaths"),
+                            "./scripts": buildProject.getProperty("node.scriptPaths")
+                        }
                     }
                 }),
                 targetTask('generateBugPackRegistry', {
