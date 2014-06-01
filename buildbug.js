e4f8704dd5a798a02e1a73c6acf96c2928c27f3d
Brian Neisler <neisler.brian@gmail.com>
2014-06-01 16:41:45 -0700

    Update version to 0.2.8

--- buildbug.js
+++ buildbug.js
@@ -44,7 +44,7 @@ var uglifyjs            = enableModule("uglifyjs");
 //-------------------------------------------------------------------------------
 
 var name                = "bugcore";
-var version             = "0.2.7";
+var version             = "0.2.8";
 var dependencies        = {
     bugpack: "0.1.12"
 };
