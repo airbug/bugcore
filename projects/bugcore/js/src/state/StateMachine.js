ce490e3a081afc9d0c4a00b35a2782ae66d75f56
Brian Neisler <neisler.brian@gmail.com>
2014-07-07 10:37:27 -0400

    Add StateMachine and ValidationMachine. Incomplete go back point.

---
+++ projects/bugcore/js/src/state/StateMachine.js
@@ -0,0 +1,149 @@
+/*
+ * Copyright (c) 2014 airbug inc. http://airbug.com
+ *
+ * bugcore may be freely distributed under the MIT license.
+ */
+
+
+//-------------------------------------------------------------------------------
+// Annotations
+//-------------------------------------------------------------------------------
+
+//@Export('StateMachine')
+
+//@Require('Class')
+//@Require('EventDispatcher')
+//@Require('Exception')
+//@Require('Set')
+//@Require('StateEvent')
+
+
+//-------------------------------------------------------------------------------
+// Context
+//-------------------------------------------------------------------------------
+
+require('bugpack').context("*", function(bugpack) {
+
+    //-------------------------------------------------------------------------------
+    // BugPack
+    //-------------------------------------------------------------------------------
+
+    var Class               = bugpack.require('Class');
+    var EventDispatcher     = bugpack.require('EventDispatcher');
+    var Exception           = bugpack.require('Exception');
+    var Set                 = bugpack.require('Set');
+    var StateEvent          = bugpack.require('StateEvent');
+
+
+    //-------------------------------------------------------------------------------
+    // Declare Class
+    //-------------------------------------------------------------------------------
+
+    /**
+     * @class
+     * @extends {EventDispatcher}
+     */
+    var StateMachine = Class.extend(EventDispatcher, /** @lends {StateMachine.prototype} */{
+
+        _name: "StateMachine",
+
+
+        //-------------------------------------------------------------------------------
+        // Constructor
+        //-------------------------------------------------------------------------------
+
+        /**
+         * @constructs
+         * @param {{
+         *      states: Array.<string>,
+         *      initialState: string
+         * }} stateMachineConfig
+         */
+        _constructor: function(stateMachineConfig) {
+
+            this._super();
+
+
+            //-------------------------------------------------------------------------------
+            // Private Properties
+            //-------------------------------------------------------------------------------
+
+            /**
+             * @private
+             * @type {Set.<string>}
+             */
+            this.availableStateSet  = new Set(stateMachineConfig.states);
+
+            /**
+             * @private
+             * @type {string}
+             */
+            this.currentState       = stateMachineConfig.initialState;
+        },
+
+
+        //-------------------------------------------------------------------------------
+        // Getters and Setters
+        //-------------------------------------------------------------------------------
+
+        /**
+         * @return {Set.<string>}
+         */
+        getAvailableStateSet: function() {
+            return this.availableStateSet;
+        },
+
+        /**
+         * @return {string}
+         */
+        getCurrentState: function() {
+            return this.currentState;
+        },
+
+
+        //-------------------------------------------------------------------------------
+        // Public Methods
+        //-------------------------------------------------------------------------------
+
+        /**
+         * @param {string} state
+         */
+        changeState: function(state) {
+            if (!this.availableStateSet.contains(state)) {
+                throw new Exception("StateDoesNotExist", {}, "state '" + state + "' does not exist in the StateMachine");
+            }
+            if (this.currentState !== state) {
+                var previousState = this.currentState;
+                this.dispatchStateChanged(previousState);
+            }
+        },
+
+        /**
+         * @param {string} state
+         * @return {boolean}
+         */
+        isState: function(state) {
+            return this.currentState === state;
+        },
+
+
+        //-------------------------------------------------------------------------------
+        // Private Methods
+        //-------------------------------------------------------------------------------
+
+        /**
+         * @private
+         * @param {string} previousState
+         */
+        dispatchStateChanged: function(previousState) {
+            this.dispatchEvent(new StateEvent(StateEvent.EventTypes.STATE_CHANGED, previousState, this.currentState));
+        }
+    });
+
+
+    //-------------------------------------------------------------------------------
+    // Exports
+    //-------------------------------------------------------------------------------
+
+    bugpack.export('StateMachine', StateMachine);
+});
---
+++ projects/bugcore/js/src/state/events/StateEvent.js
@@ -0,0 +1,116 @@
+/*
+ * Copyright (c) 2014 airbug inc. http://airbug.com
+ *
+ * bugcore may be freely distributed under the MIT license.
+ */
+
+
+//-------------------------------------------------------------------------------
+// Annotations
+//-------------------------------------------------------------------------------
+
+//@Export('StateEvent')
+
+//@Require('Class')
+//@Require('Event')
+
+
+//-------------------------------------------------------------------------------
+// Context
+//-------------------------------------------------------------------------------
+
+require('bugpack').context("*", function(bugpack) {
+
+    //-------------------------------------------------------------------------------
+    // BugPack
+    //-------------------------------------------------------------------------------
+
+    var Class   = bugpack.require('Class');
+    var Event   = bugpack.require('Event');
+
+
+    //-------------------------------------------------------------------------------
+    // Declare Class
+    //-------------------------------------------------------------------------------
+
+    /**
+     * @class
+     * @extends {Event}
+     */
+    var StateEvent = Class.extend(Event, /** @lends {StateEvent.prototype} */{
+
+        _name: "StateEvent",
+
+
+        //-------------------------------------------------------------------------------
+        // Constructor
+        //-------------------------------------------------------------------------------
+
+        /**
+         * @constructs
+         * @param {string} type
+         * @param {string} previousState
+         * @param {string} currentState
+         */
+        _constructor: function(type, previousState, currentState) {
+
+            this._super();
+
+
+            //-------------------------------------------------------------------------------
+            // Private Properties
+            //-------------------------------------------------------------------------------
+
+            /**
+             * @private
+             * @type {string}
+             */
+            this.currentState   = currentState;
+
+            /**
+             * @private
+             * @type {string}
+             */
+            this.previousState  = previousState;
+        },
+
+
+        //-------------------------------------------------------------------------------
+        // Getters and Setters
+        //-------------------------------------------------------------------------------
+
+        /**
+         * @return {string}
+         */
+        getCurrentState: function() {
+            return this.currentState;
+        },
+
+        /**
+         * @return {string}
+         */
+        getPreviousState: function() {
+            return this.previousState;
+        }
+    });
+
+
+    //-------------------------------------------------------------------------------
+    // Static Properties
+    //-------------------------------------------------------------------------------
+
+    /**
+     * @static
+     * @enum {string}
+     */
+    StateEvent.EventTypes = {
+        STATE_CHANGED: "StateEvent:EventTypes:StateChanged"
+    };
+
+
+    //-------------------------------------------------------------------------------
+    // Exports
+    //-------------------------------------------------------------------------------
+
+    bugpack.export('StateEvent', StateEvent);
+});
