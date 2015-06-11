18e18e75ee00f3a90f54456a36a019e1ecbf3b37
Brian Neisler <brian@airbug.com>
2015-06-11 11:47:12 -0700

    Bump version to 0.3.0

--- projects/bugcore-web/js/scripts/bugcore-web.js
+++ projects/bugcore-web/js/scripts/bugcore-web.js
@@ -12,5 +12,5 @@
 (function(window) {
     var bugpack     = require("bugpack").context();
     var BugCore     = bugpack.require("BugCore");
-    window.bugcore = window.bugcore || BugCore.getInstance();
+    window.bugcore  = window.bugcore || BugCore.getInstance();
 })(window);
