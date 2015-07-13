acafd69d81c8cb2ef5a8db61c988e912533b82ff
Brian Neisler <brian@airbug.com>
2015-07-13 12:31:48 -0700

    Bump version to 0.3.3

--- buildbug.js
+++ buildbug.js
@@ -46,7 +46,7 @@ var uglifyjs            = enableModule("uglifyjs");
 //-------------------------------------------------------------------------------
 
 var name                = "bugcore";
-var version             = "0.3.2";
+var version             = "0.3.3";
 var dependencies        = {
     bugpack: "0.2.0"
 };
